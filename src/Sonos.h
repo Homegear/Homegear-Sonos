3b42206510487913af061e26f68b929ff6f618e6
Sathya Laufer <sathya@laufers.net>
2019-01-12 17:53:35 +0100

    Base library changes

--- src/Sonos.h
+++ src/Sonos.h
@@ -41,7 +41,7 @@ using namespace BaseLib;
 class Sonos : public BaseLib::Systems::DeviceFamily
 {
 public:
-	Sonos(BaseLib::SharedObjects* bl, BaseLib::Systems::DeviceFamily::IFamilyEventSink* eventHandler);
+	Sonos(BaseLib::SharedObjects* bl, BaseLib::Systems::IFamilyEventSink* eventHandler);
 	virtual ~Sonos();
 	virtual void dispose();
 
